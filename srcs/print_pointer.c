423037dab44fa72b16691f2baf286502471cad41
justcasino <justincasino@gmail.com>
2017-04-03 08:43:05 -0700

    commenting out line 33-37

--- srcs/print_pointer.c
+++ srcs/print_pointer.c
@@ -30,11 +30,11 @@ void ft_putpnt(t_format *format, t_conversion *conversion, uintmax_t num,
       print_extra_width(format, conversion, num_space, pad, num_len);
     ft_putstr("0x");
     format->num_writt += 2;
-    if (str_num) //this what you were testing DELETE THIS ******
-    {
-        write(1, "0",1 );
-        format->num_writt++;
-    }
+//    if (str_num) //this what you were testing DELETE THIS ******
+ //   {
+  //      write(1, "0",1 );
+   //     format->num_writt++;
+    //}
    while (str_num[i])
    {
        write(1, &str_num[i], 1);
